86bfa766e496ef2aacba7481a7a68917ff86f13d
Neha Utekar <neha.utekar2608@gmail.com>
2020-06-28 01:32:20 +0530

    CR:Neha
    Added new method for fetching movie details

--- app/src/main/java/com/ort/moviebrowserapp/APIConfiguration/APIManagement.kt
+++ app/src/main/java/com/ort/moviebrowserapp/APIConfiguration/APIManagement.kt
@@ -1,8 +1,10 @@
 package com.ort.moviebrowserapp.APIConfiguration
 
 import com.ort.moviebrowserapp.pojo.MovieBrowserResponsePojo
+import com.ort.moviebrowserapp.pojo.MovieDetailsPojo
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
@@ -18,6 +20,11 @@ interface APIManagement {
     @GET("search/movie")
     fun getSearchResult(@Query("api_key") api_key: String?,
                     @Query("query") searchQuery:String?): Call<MovieBrowserResponsePojo?>?
+
+    //to get movie details
+    @GET("movie/{movie_id}")
+    fun getMovieDetails(@Path("movie_id")movieId:Long,
+                        @Query("api_key") api_key: String?): Call<MovieDetailsPojo?>?
 
 
 }
